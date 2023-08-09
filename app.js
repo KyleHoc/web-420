bc2d9704770fa82a7bc5fe6922565a4af555b552
Kyle Hochdoerfer <kjhoch2@gmail.com>
2023-08-09 11:20:55 -0400

    Week 1 complete

---
+++ app.js
@@ -0,0 +1,37 @@
+//Enable strict mode
+"use strict";
+
+//Require express, mongoose, http, swagger-ui-express and swagger-jsdoc
+const express = require('express');
+const mongoose = require('mongoose');
+const http = require('http');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+
+//Create an app variable set to the express library
+const app = express();
+
+//Set the port
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "WEB 420 RESTful APIs",
+            version: "1.0.0",
+        },
+    },
+    apis: ['./routes/*.js'],
+};
+
+const openapiSpecification = swaggerJsdoc(options);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+
+app.listen(PORT, () => {
+    console.log('Application started and listening on PORT ' + PORT);
+});

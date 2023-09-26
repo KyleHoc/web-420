942b2bc3adc3691f4c64c320e443d2423513c4d7
Kyle Hochdoerfer <kjhoch2@gmail.com>
2023-09-26 15:44:19 -0400

    Week 8 complete

--- routes/hochdoerfer-composer-routes.js
+++ routes/hochdoerfer-composer-routes.js
@@ -166,5 +166,144 @@ router.post('/composers', async(req, res) => {
     }
 })
 
+/**
+ * updateComposerById
+ * @openapi
+ * /api/composers/{id}:
+ *   put:
+ *     tags:
+ *       - Composers
+ *     name: updateComposerById
+ *     description: API for updating an existing document in MongoDB.
+ *     summary: Updates a document in MongoDB. 
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: Id to filter the collection by. 
+ *         schema: 
+ *           type: string
+ *     requestBody:
+ *       description: Updated field
+ *       content:
+ *         application/json:
+ *           schema:
+ *             required:
+ *               - firstName
+ *               - lastName
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *               lastName:
+ *                 type: string
+ *     responses:
+ *       '200':
+ *         description: Composer updated
+ *       '401':
+ *         description: Invalid composerId
+ *       '500':
+ *         description: Server Exception
+ *       '501':
+ *         description: MongoDB Exception
+ */
+router.put('/composers/:id', async (req, res) => {
+    try {
+        //Store the composer id from the request as a variable
+        const composerDocId = req.params.id;
+
+        //Find the composer with the id matching the composerDocId
+        Composer.findOne({'_id': composerDocId}, function(err, composer) {
+            //If the requested composer document is found
+            if(composer){
+                //Set the composer's first and last name to the values provided in the request
+                composer.set({
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName
+                });
+
+                //Save the updated composer document
+                composer.save(function(err, updatedComposer) {
+                    if (err) {
+                        //If MongoDB encounters an error, print it to the console and send it as a response
+                        console.log(err);
+                        res.status(501).send({
+                            'message': `MongoDB Exception: ${err}`
+                        })
+                    } else {
+                        //Otherwise, output the updated composer info and send it as a response
+                        console.log(updatedComposer);
+                        res.json(updatedComposer);
+                    }
+                })
+            } else {
+                //If the requested composer document is not found, output an error stating so and send it as a response
+                console.log("Error: Invalid Composer ID");
+                res.status(401).send({
+                    'message': `Invalid composerID`
+                })
+            }
+        }) 
+    } catch (e) {
+        //If the server encounters an error, output it to the console and send it as a response
+        console.log(e);
+        res.status(500).send({
+            'message': `Server Exception: ${e.message}`
+        })
+    }
+})
+
+/**
+ * deleteComposerById
+ * @openapi
+ * /api/composers/{id}:
+ *   delete:
+ *     tags:
+ *       - Composers
+ *     name: deleteComposerById
+ *     description: API for deleting a document from MongoDB.
+ *     summary: Removes a document from MongoDB.
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: Id of the document to remove. 
+ *         schema: 
+ *           type: string
+ *     responses:
+ *       '200':
+ *         description: Composer deleted
+ *       '500':
+ *         description: Server Exception
+ *       '501':
+ *         description: MongoDB Exception
+ */
+router.delete('/composers/:id', async (req, res) => {
+    try {
+        //Store the requested composer ID as a variable
+        const composerDocId = req.params.id;
+
+        //Find and delete the composer document with the requested ID
+        Composer.findByIdAndDelete({'_id': composerDocId}, function(err, composer) {
+            if (err) {
+                //If MongoDB encounters an error, output it to the console and send it as a response
+                console.log(err);
+                res.status(501).send({
+                    'message': `MongoDB Exception: ${err}`
+                })
+            } else {
+                //If the document is successfully deleted, output it to the console and send it as a response
+                console.log(composer);
+                res.json(composer);
+            }
+        })
+    } catch (e) {
+        //If the server encounters an error, output it to the console and send it as a response
+        console.log(e);
+        res.status(500).send({
+            'message': `Server Exception: ${e.message}`
+        })
+    }
+})
+
 //Export module
 module.exports = router;
